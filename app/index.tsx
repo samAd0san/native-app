8c2b1d27d294af8a50b4ece5d7f0c34baf28a8f7
Sohaib samad <sohaibsamad.hala@gmail.com>
2025-01-08 19:53:07 +0530

    Setup Fonts and Assets 1. Added Splash Screen. 2. Added custom fonts and color <- tailwind.config.js. 3. Added static data <- constants. 4. Added image.d.ts to resolve compatibility issue in tsx.

--- app/index.tsx
+++ app/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold text-4xl text-green-600">بسم الله!</Text>
+      <Text className="font-rukik-extrabold text-4xl text-primary-300">Index.tsx</Text>
       <Link href="/sign-in"> Sign In </Link>
       <Link href="/profile"> Profile </Link>
 
--- app/profile.tsx
+++ app/profile.tsx
@@ -4,7 +4,7 @@ import { Text, View} from 'react-native';
 const Profile = () => {
     return (
         <View>
-            <Text>Profile</Text>
+            <Text>WELCOME TO YOU PROFILE</Text>
         </View>
     )
 }
